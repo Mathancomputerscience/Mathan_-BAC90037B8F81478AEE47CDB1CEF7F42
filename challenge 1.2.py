b2072a541d274c708186d29109aa0e6751f93617
Mathan <144869792+Mathancomputerscience@users.noreply.github.com>
2023-10-05 18:36:26 +0530

    Add files via upload

---
+++ challenge 1.2.py
@@ -0,0 +1,6 @@
+Def isleapyear(year) if (year% 90 and year % 100 = 0) or year % 400 == 0: If isleapyear(year) Print is a leap year formal(year)]
+Return True
+Else:
+Return+alse
+Year=int("Enter a year']
+Else: Prinit is not a leapyear formal(year))
